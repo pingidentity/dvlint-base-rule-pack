344d9cc551b4edda6619af0145182fa0ac1aeb69
Manish Singh <msingh@pingidentity.com>
2025-04-24 15:07:07 +0530

    rules segregation and unit tests

---
+++ rules/dv-rule-AEqualsBMultipleCondition-001/AEqualsBMultipleConditionRule.js
@@ -0,0 +1,88 @@
+const { LintRule } = require("@ping-identity/dvlint");
+
+class AEqualsBMultipleConditionRule extends LintRule {
+    constructor() {
+        super({
+            id: "dv-rule-AEqualsBMultipleCondition-001",
+            description: "Invalid configurations for 'A == B (Multiple Conditions)' capability",
+            cleans: false,
+            reference: "",
+        });
+
+        this.addCode("dv-er-AEqualsBMultipleCondition-001", {
+            description: "Invalid configurations for 'A == B (Multiple Conditions)' capability",
+            message: "Invalid configurations for 'A == B (Multiple Conditions)' capability",
+            type: "error",
+            recommendation: "Define at least one expected outcome values for the 'A == B (Multiple Conditions)' capability to ensure correct flow branching.",
+        });
+        this.addCode("dv-bp-AEqualsBMultipleCondition-002", {
+            description: "Invalid output mapping for 'A == B (Multiple Conditions)' capability",
+            message: "Invalid output mapping for 'A == B (Multiple Conditions)' capability",
+            type: "best-practice",
+            recommendation: "Map each specific outcome value from 'A == B (Multiple Conditions)' to the next node. Do not use generic node-level output.",
+        });
+        this.addCode("dv-er-AEqualsBMultipleCondition-003", {
+            description: "Unmapped outcome(s) in 'A == B (Multiple Conditions)' capability",
+            message: "Unmapped outcome(s) in 'A == B (Multiple Conditions)' capability",
+            type: "error",
+            recommendation: "Map the outcome ‘%’ to an appropriate next node in the ‘A == B (Multiple Conditions)’ capability.",
+        });
+
+    }
+
+    runRule() {
+        try {
+            for (const flow of this.allFlows) {
+                const { nodes, edges } = flow?.graphData?.elements
+                const multiValueSourceId = edges?.filter(edge => edge.data.multiValueSourceId).map(ed => ed.data.multiValueSourceId) || [];
+
+                nodes?.forEach((node) => {
+                    const { data } = node;
+                    //check 'A == B (Multiple Conditions)' capability(Functions)  has atleast one expected outcome value
+                    if (data.nodeType === "CONNECTION" && (data.capabilityName === "AEqualsMultipleB")) {
+                        let leftValueA = data.properties?.leftValueA?.value;
+                        let rightMultipleValuesArr = data.properties?.rightValueMultiple?.value || [];
+                        if (leftValueA && rightMultipleValuesArr.length === 0) {
+                            this.addError("dv-er-AEqualsBMultipleCondition-001", {
+                                flowId: flow.flowId,
+                                nodeId: data.id,
+                            });
+                        }
+
+                        //check if 'A == B (Multiple Conditions)' conditions  is properly configured
+                        let missingConditionNames = [];
+                        rightMultipleValuesArr?.forEach((rightValue) => {
+                            if (rightValue.id && !multiValueSourceId.includes(rightValue.id)) {
+                                let conditionName = JSON.parse(rightValue.value)[0].children[0].text
+                                missingConditionNames.push(conditionName);
+                            }
+                        })
+                        if (missingConditionNames.length > 0) {
+                            this.addError("dv-er-AEqualsBMultipleCondition-003", {
+                                flowId: flow.flowId,
+                                nodeId: data.id,
+                                recommendationArgs: [missingConditionNames.join(',')]
+                            });
+                        }
+
+                        //check 'A == B (Multiple Conditions)' capability(Functions) node has any node connected to main node, other than the conditon nodes
+                        let nonConditionEvalNodeArr = edges?.filter(edge => edge.data.source === data.id && !edge?.data.multiValueSourceId);
+                        if (nonConditionEvalNodeArr.length > 0) {
+                            let nonConditionEvalNode = nonConditionEvalNodeArr[0].data.target;
+                            let nonConditonConnectorNodeId = edges?.filter(edge => edge.data.source === nonConditionEvalNode)[0].data.target;
+                            const connectorNode = nodes?.filter(node => node.data.id === nonConditonConnectorNodeId);
+                            this.addError("dv-bp-AEqualsBMultipleCondition-002", {
+                                flowId: flow.flowId,
+                                nodeId: connectorNode[0].data.id,
+                            });
+                        }
+                    }
+                });
+            }
+        } catch (err) {
+            this.addError(undefined, { messageArgs: [`${err}`] });
+        }
+    }
+}
+
+module.exports = AEqualsBMultipleConditionRule;
---
+++ rules/dv-rule-validFields-001/ValidFieldsRule.js
@@ -0,0 +1,106 @@
+const { LintRule } = require("@ping-identity/dvlint");
+
+class ValidFields extends LintRule {
+    constructor() {
+        super({
+            id: "dv-rule-validFields-001",
+            description: "Missing or Incorrect fields in nodes",
+            cleans: false,
+            reference: "",
+        });
+
+        this.addCode("dv-er-validFields-001", {
+            description: 'Missing site key configuration for reCAPTCHA',
+            message: 'Missing site key configuration for reCAPTCHA',
+            type: "error",
+            recommendation: "Configure the required site key value to enable proper reCAPTCHA functionality.",
+        });
+
+        this.addCode("dv-er-validFields-002", {
+            description: "Incorrect 'Additional Fields in the Response' in Send Success JSON Response capability",
+            message: "Incorrect 'Additional Fields in the Response' in Send Success JSON Response capability",
+            type: "error",
+            recommendation: "Ensure valid key/value pairs are configured under 'Additional Fields in the Response'.",
+        });
+
+        this.addCode("dv-er-validFields-003", {
+            description: "Incorrect 'Additional Fields in the Response' in Send Error JSON Response capability",
+            message: "Incorrect 'Additional Fields in the Response' in Send Error JSON Response capability",
+            type: "error",
+            recommendation: "Ensure valid key/value pairs are configured under 'Additional Fields in the Response'.",
+        });
+
+        this.addCode("dv-er-validFields-004", {
+            description: "Missing 'Property Name' for Output Fields in Custom HTML Template capability",
+            message: "Missing 'Property Name' for Output Fields in Custom HTML Template capability",
+            type: "error",
+            recommendation: "Add a 'Property Name' to define the purpose of the output field. This property can then be referenced by successive nodes in the flow.",
+        });
+
+    }
+
+    runRule() {
+        try {
+            for (const flow of this.allFlows) {
+                const { nodes, edges } = flow?.graphData?.elements
+                nodes?.forEach((node) => {
+                    const { data } = node;
+
+                    //check if skrecpatcha in custom HTMLTemplate or customHtmlMessage is properly configured
+                    if (data.nodeType === "CONNECTION" && (data.capabilityName === "customHTMLTemplate" || data.capabilityName === "customHtmlMessage")) {
+                        let content = data.properties?.message?.value || data.properties?.customHTML?.value || "[]";
+                        if (content.indexOf('skrecaptcha') > -1) {
+                            let message = JSON.parse(content);
+                            const value = message[0]?.children?.filter(d => d.component === 'skrecaptcha' && d.options);
+                            if (value.length > 0 && !value[0]?.options?.siteKey) {
+                                this.addError("dv-er-validFields-001", {
+                                    flowId: flow.flowId,
+                                    recommendationArgs: [data.id],
+                                    nodeId: data.id,
+                                });
+                            }
+                        }
+
+                        if (data.capabilityName === "customHTMLTemplate") {
+                            //check if output field propertyName field has no value
+                            let formFieldsListArr = data.properties?.formFieldsList?.value;
+                            let emptyProperty = []
+                            formFieldsListArr?.forEach((formField) => {
+                                if (!formField.propertyName) {
+                                    emptyProperty.push(formField?.preferredControlType);
+                                }
+                            });
+
+                            if (emptyProperty.length > 0) {
+                                this.addError("dv-er-validFields-004", {
+                                    flowId: this.mainFlow.flowId,
+                                    recommendationArgs: [emptyProperty.join(',')],
+                                    nodeId: data.id,
+                                });
+                            }
+                        }
+                    }
+
+                    //check if claimsNameValuePairs in  createErrorResponse or createSuccessResponse is properly configured
+                    if (data.nodeType === "CONNECTION" && (data.capabilityName === "createErrorResponse" || data.capabilityName === "createSuccessResponse")) {
+                        let value = data.properties?.claimsNameValuePairs?.value || [];
+                        const noValFlag = value?.filter(v => !v.value || !JSON.parse(v.value)[0].children[0].text).length > 0;
+                        const errorCode = data.capabilityName === "createErrorResponse" ? "dv-er-validFields-003" : "dv-er-validFields-002";
+                        if (!value || noValFlag) {
+                            this.addError(errorCode, {
+                                flowId: flow.flowId,
+                                nodeId: data.id,
+                            });
+                        }
+                    }
+
+
+                });
+            }
+        } catch (err) {
+            this.addError(undefined, { messageArgs: [`${err}`] });
+        }
+    }
+}
+
+module.exports = ValidFields;
