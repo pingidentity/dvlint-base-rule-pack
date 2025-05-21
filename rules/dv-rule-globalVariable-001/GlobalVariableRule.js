3043e731672014dc35a0c6b9533962ae3f47ae58
msingh-developer <133210848+msingh-developer@users.noreply.github.com>
2025-05-21 22:14:25 +0530

    Connector configuration, global variable check (#37)
    * Connector configuration, gloabl variable rule
    
    * add more customScript option of securIdConnector
    
    * Tests for lexisnexis and securId

--- rules/dv-rule-globalVariable-001/GlobalVariableRule.js
+++ rules/dv-rule-globalVariable-001/GlobalVariableRule.js
@@ -25,7 +25,7 @@ class GlobalVariableRule extends LintRule {
             description: "Custom JavaScript usage in flow",
             message: "Custom JavaScript usage in flow",
             type: "best-practice",
-            recommendation: "Review the script in the '%' capability to ensure it does not introduce security vulnerabilities.",
+            recommendation: "Review the script in the % to ensure it does not introduce security vulnerabilities.%",
         });
 
     }
@@ -48,21 +48,30 @@ class GlobalVariableRule extends LintRule {
                     documentVerification: 'htmlConfig_docVerificationConsentScreen'
                 },
                 userPolicyConnector: {
-                    authenticateUser: 'htmlConfig_htmlLogin1'
+                    authenticateUser: ['htmlConfig_htmlLogin1', 'htmlConfig_htmlLogin2']
                 },
                 idemiaConnector: {
-                    documentVerification: 'htmlConfig_docVerificationDocumentTypeSelectScreen',
-                    portraitVerification: 'htmlConfig_portraitVerificationCheckBrowserSupport'
+                    documentVerification: [
+                        'htmlConfig_docVerificationDocumentTypeSelectScreen',
+                        'htmlConfig_docVerificationConnectivityCheck',
+                        'htmlConfig_docVerificationDocCapture',
+                        'htmlConfig_docVerificationCountrySelectScreen',
+                        'htmlConfig_docVerificationFrontScanTutorial'
+                    ],
+                    portraitVerification: [
+                        'htmlConfig_portraitVerificationCheckBrowserSupport',
+                        'htmlConfig_portraitVerificationPortraitCapture'
+                    ]
                 },
                 jumioConnector: {
-                    register: 'htmlConfig_documentVerify3',
+                    register: ['htmlConfig_documentVerify3', 'htmlConfig_documentVerify2'],
                     documentVerify: 'htmlConfig_documentVerify3',
                     login: 'htmlConfig_login2',
-                    loginFirstFactor: 'htmlConfig_loginFirstFactor1'
+                    loginFirstFactor: ['htmlConfig_loginFirstFactor1', 'htmlConfig_loginFirstFactor2']
                 },
                 nuanceConnector: {
-                    register: 'htmlConfig_htmlRegister1',
-                    login: 'htmlConfig_htmlLogin1'
+                    register: ['htmlConfig_htmlRegister1', 'htmlConfig_htmlRegister2'],
+                    login: ['htmlConfig_htmlLogin1', 'htmlConfig_htmlLogin2']
                 },
                 pingFederateConnectorV2: {
                     authenticate: ''
@@ -74,34 +83,47 @@ class GlobalVariableRule extends LintRule {
                 },
                 entrustConnector: {
                     auth: 'htmlConfig0_select'
+                },
+                securIdConnector: {
+                    verifyRSA: ['htmlConfig0_selectScreen', 'htmlConfig1_authenticateTokenCode', 'htmlConfig2_emergencyTokenCode']
+                },
+                lexisnexisV2Connector: {
+                    initialize: 'htmlConfig_initializeScreen',
+                    otp: 'htmlConfig_OTPscreen',
+                    iidqa: 'htmlConfig_questionsScreen2'
                 }
             }
 
-            const connectorIdWithKeyBeforeCustomScript = ['transunionConnector','userPolicyConnector','idemiaConnector','jumioConnector','nuanceConnector','entrustConnector'];
-            const connectorIdWithoutKeyBeforeCustomScript = ['pingFederateConnectorV2','mfaContainerConnector'];
 
+            const connectorIdWithKeyBeforeCustomScript = [
+                'transunionConnector', 'userPolicyConnector',
+                'idemiaConnector', 'jumioConnector',
+                'nuanceConnector', 'entrustConnector',
+                'lexisnexisV2Connector', 'securIdConnector'
+            ];
+            const connectorIdWithoutKeyBeforeCustomScript = ['pingFederateConnectorV2', 'mfaContainerConnector'];
+            const pattern = /\{\{(.*?)\}\}/g; // regex pattern to match all instances of {{...}}
             for (const flow of this.allFlows) {
                 const { nodes, edges } = flow?.graphData?.elements
                 nodes?.forEach((node) => {
                     const { data } = node;
-                    const pattern = /\{\{(.*?)\}\}/g; // regex pattern to match all instances of {{...}}
-
                     if (data.nodeType === "CONNECTION" && data.capabilityName === "customHTMLTemplate" || data.capabilityName === "customFunction") {
                         let property = data.capabilityName === "customFunction" ? "code" : "customScript";
                         let value = data.properties[property]?.value;
-                        const uniqueGlobalVariables = this.getPropertiesValue(value, pattern)
+                        const uniqueGlobalVariables = this.getPropertiesValue(value, pattern);
                         const globalVarArr = []
                         uniqueGlobalVariables?.forEach(v => {
-                            // Check if the variable is a global variable
                             if (v.includes('global.')) {
                                 globalVarArr.push(`{{${v}}}`)
                             }
                         });
-                        let ruleNo = data.capabilityName === "customFunction" ? "dv-bp-globalVariable-002" : "dv-bp-globalVariable-001";
-                        this.addError(ruleNo, {
-                            flowId: this.mainFlow.flowId,
-                            nodeId: node.data.id
-                        });
+                        if (globalVarArr.length > 0) {
+                            let ruleNo = data.capabilityName === "customFunction" ? "dv-bp-globalVariable-002" : "dv-bp-globalVariable-001";
+                            this.addError(ruleNo, {
+                                flowId: this.mainFlow.flowId,
+                                nodeId: node.data.id
+                            });
+                        }
                     }
 
                     if (
@@ -109,10 +131,34 @@ class GlobalVariableRule extends LintRule {
                         (data.connectorId in connectorIdsForGlobalVariableCheck) &&
                         data.capabilityName in connectorIdsForGlobalVariableCheck[data.connectorId]
                     ) {
-                        let value
+                        let value;
                         if (connectorIdWithKeyBeforeCustomScript.includes(data.connectorId)) {
                             const key = connectorIdsForGlobalVariableCheck[data.connectorId][data.capabilityName];
-                            value = data.properties[key]?.properties?.customScript?.value;
+                            // this case is for capabilities where multiple code snippet sections are available in custom screens
+                            if (Array.isArray(key) && key.length > 0) {
+                                const globalVarArr = []
+                                key.forEach(k => {
+                                    const propValue = data.properties[k]?.properties?.customScript?.value;
+                                    if (propValue) {
+                                        const uniqueGlobalVariables = this.getPropertiesValue(propValue, pattern)
+                                        uniqueGlobalVariables?.forEach(v => {
+                                            if (v.includes('global.')) {
+                                                globalVarArr.push(`{{${v}}}`)
+                                            }
+                                        });
+                                    }
+                                });
+                                if (globalVarArr.length > 0) {
+                                    this.addError("dv-bp-globalVariable-003", {
+                                        flowId: this.mainFlow.flowId,
+                                        nodeId: node.data.id,
+                                        recommendationArgs: [`'${data.capabilityName}' capability`, '']
+                                    });
+                                }
+                            } else {
+                                value = data.properties[key]?.properties?.customScript?.value;
+                            }
+
                         } else if (connectorIdWithoutKeyBeforeCustomScript.includes(data.connectorId)) {
                             value = data.properties?.customScript?.value;
                         }
@@ -120,18 +166,45 @@ class GlobalVariableRule extends LintRule {
                         const uniqueGlobalVariables = this.getPropertiesValue(value, pattern)
                         const globalVarArr = []
                         uniqueGlobalVariables?.forEach(v => {
-                            // Check if the variable is a global variable
                             if (v.includes('global.')) {
                                 globalVarArr.push(`{{${v}}}`)
                             }
                         });
-                        this.addError("dv-bp-globalVariable-003", {
-                            flowId: this.mainFlow.flowId,
-                            nodeId: node.data.id,
-                            recommendationArgs: [data.capabilityName]
-                        });
+                        if (globalVarArr.length > 0) {
+                            this.addError("dv-bp-globalVariable-003", {
+                                flowId: this.mainFlow.flowId,
+                                nodeId: node.data.id,
+                                recommendationArgs: [`'${data.capabilityName}' capability`, '']
+                            });
+                        }
                     }
                 });
+
+
+                // configuration code check
+                const connectorIdsForConfigurationCheck = ['codeSnippetConnector'];
+                connectorIdsForConfigurationCheck.forEach(connectorId => {
+                    const codeSnippetConnectorNodeids = nodes?.filter(node => node.data.connectorId === connectorId).map(node => node.data.id);
+                    flow.connections?.forEach((connection) => {
+                        if (connection?.connectorId === connectorId) {
+                            const value = connection?.properties?.code?.value;
+                            const uniqueGlobalVariables = this.getPropertiesValue(value, pattern);
+                            const globalVarArr = []
+                            uniqueGlobalVariables?.forEach(v => {
+                                // Check if the configurations code section has global variable
+                                if (v.includes('global.')) {
+                                    globalVarArr.push(`{{${v}}}`)
+                                }
+                            });
+                            if (globalVarArr.length > 0) {
+                                this.addError("dv-bp-globalVariable-003", {
+                                    flowId: this.mainFlow.flowId,
+                                    recommendationArgs: [`'${connectorId}' connector`, `This connector is used in the following node(s): ${codeSnippetConnectorNodeids.join(',')}.`],
+                                });
+                            }
+                        }
+                    })
+                })
             }
         } catch (err) {
             this.addError(undefined, { messageArgs: [`${err}`] });
