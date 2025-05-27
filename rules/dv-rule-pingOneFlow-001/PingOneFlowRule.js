b9d770df533d8034c4ced078eaf0850352b4da4b
msingh-developer <133210848+msingh-developer@users.noreply.github.com>
2025-05-27 20:04:53 +0530

    handle trigger complete branch in p1FalsePathRule (#41)

--- rules/dv-rule-pingOneFlow-001/PingOneFlowRule.js
+++ rules/dv-rule-pingOneFlow-001/PingOneFlowRule.js
@@ -73,22 +73,25 @@ class PingOneFlowRule extends LintRule {
 
           const falseBranchCount = evalNodeArr?.filter(d => d === 'allTriggersFalse' || d === "anyTriggersFalse").length;
           const trueBranchCount = evalNodeArr?.filter(d => d === 'allTriggersTrue' || d === "anyTriggersTrue").length;
-
-          if (
-            ((targetFromEvalNodes.length >= 2 || connectedNodes.length >= 2) && (!trueBranchCount || trueBranchCount !== 1) && falseBranchCount !== 1) ||   // in case of singleEval has multiple branches
-            ((connectedNodes.length === 1 && targetFromEvalNodes.length === 1) && falseBranchCount === 1) ||                                                // if only one false branch
-            ((connectedNodes.length === 1 && targetFromEvalNodes.length === 1) && (trueBranchCount === 1 || trueBranchCount === 0)) ||                      // if only one true branch
-            (connectedNodes.length >= 2 && !(trueBranchCount >= 1) && !(falseBranchCount >= 1)) ||                                                          // if not at least one false branch and one true branch
-            (connectedNodes.length >= 2 && trueBranchCount >= 1 && falseBranchCount !== 1) ||                                                               // if all true branches
-            (connectedNodes.length >= 2 && falseBranchCount >= 1 && (trueBranchCount && trueBranchCount !== 1))                                             // if all false branches
-          ) {
-            this.addError("dv-er-pingOneFlow-002", {
-              flowId: this.mainFlow.flowId,
-              recommendationArgs: [node.data.capabilityName],
-              messageArgs: [node.data.capabilityName],
-              nodeId: node.data.id,
-            });
+          const noCompleteTriggerBranch = evalNodeArr?.filter(d => d === 'allTriggersComplete' || d === 'always').length === 0;
+          if (noCompleteTriggerBranch) {
+            if (
+              ((targetFromEvalNodes.length >= 2 || connectedNodes.length >= 2) && (!trueBranchCount || trueBranchCount !== 1) && falseBranchCount !== 1) ||   // in case of singleEval has multiple branches
+              ((connectedNodes.length === 1 && targetFromEvalNodes.length === 1) && falseBranchCount === 1) ||                                                // if only one false branch
+              ((connectedNodes.length === 1 && targetFromEvalNodes.length === 1) && (trueBranchCount === 1 || trueBranchCount === 0)) ||                      // if only one true branch
+              (connectedNodes.length >= 2 && !(trueBranchCount >= 1) && !(falseBranchCount >= 1)) ||                                                          // if not at least one false branch and one true branch
+              (connectedNodes.length >= 2 && trueBranchCount >= 1 && falseBranchCount !== 1) ||                                                               // if all true branches
+              (connectedNodes.length >= 2 && falseBranchCount >= 1 && (trueBranchCount && trueBranchCount !== 1))                                             // if all false branches
+            ) {
+              this.addError("dv-er-pingOneFlow-002", {
+                flowId: this.mainFlow.flowId,
+                recommendationArgs: [node.data.capabilityName],
+                messageArgs: [node.data.capabilityName],
+                nodeId: node.data.id,
+              });
+            }
           }
+
         }
       })
 
