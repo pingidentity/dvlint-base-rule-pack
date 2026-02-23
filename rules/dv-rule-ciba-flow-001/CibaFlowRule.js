33b730c0fa45cd8b97294ad9b1fc685d01eada57
Nitin Verma <nverma@pingidentity.com>
2026-02-23 17:44:37 +0530

    Minor changes as per QA and added more UTs

--- rules/dv-rule-ciba-flow-001/CibaFlowRule.js
+++ rules/dv-rule-ciba-flow-001/CibaFlowRule.js
@@ -1,5 +1,4 @@
 const { LintRule } = require("@ping-identity/dvlint");
-
 class CibaFlowRule extends LintRule {
     constructor() {
         super({
@@ -25,62 +24,69 @@ class CibaFlowRule extends LintRule {
             description: "CIBA response nodes are not allowed in subflows.",
             message: "CIBA response nodes are not allowed in subflows.",
             type: "error",
-            recommendation:"Remove the 'Return CIBA Success' and 'Return CIBA Failure' capabilities from subflow - '%'. These nodes must be present only in the main CIBA flow to ensure proper response handling.",  
+            recommendation: "Remove the 'Return CIBA Success' and 'Return CIBA Failure' capabilities from subflow - '%'. These nodes must be present only in the main CIBA flow to ensure proper response handling.",
         });
-
-    
     }
 
     runRule() {
-       try {
+        try {
             const targetFlow = this.mainFlow;
-            const isP1CibaFlow = targetFlow?.settings?.pingOneFlow && targetFlow?.settings?.cibaFlow ;
-            //These rules apply only for Pingone CIBA Flows
-            if(isP1CibaFlow){
-                for (const flow of this.allFlows) {        
-                    const { nodes } = flow?.graphData?.elements;
-                    //Get all flow connector nodes for Rule 3
-                    const  mainFlowNodes  = targetFlow?.graphData?.elements?.nodes?.filter(n => n.data.connectorId === 'flowConnector' && n.data?.isDisabled !== true);
-                    
-                    // First Rule:  logic for pingOneAuthenticationConnector capabilities
-                    const p1AuthNodes = nodes?.filter(n => n.data.connectorId === 'pingOneAuthenticationConnector' && n.data?.isDisabled !== true);
+            const isP1CibaFlow = targetFlow?.settings?.pingOneFlow && targetFlow?.settings?.cibaFlow;
+            let oobStartCapabilityPresent = false;
+            let oobContinueCapabilityPresent = false;
 
+            //These rules apply only for Pingone CIBA Flows
+            if (isP1CibaFlow) {
+
+                const mainFlowExecutionNodes = targetFlow?.graphData?.elements?.nodes?.filter(n => n.data.connectorId === 'flowConnector' && n.data?.isDisabled !== true);
+
+                for (const flow of this.allFlows) {
+
+                    const { nodes } = flow?.graphData?.elements;
+                    const p1AuthNodes = nodes?.filter(n => n.data.connectorId === 'pingOneAuthenticationConnector' && n.data?.isDisabled !== true);
                     const cibaErrorCapabilityExist = p1AuthNodes?.some(n => n.data.capabilityName === 'returnCibaError');
                     const cibaSuccessCapabilityExist = p1AuthNodes?.some(n => n.data.capabilityName === 'returnCibaSuccess');
-                    //Both returnCibaError and returnCibaSuccess are required for CIBA flow response, if any of them is missing then throw error.
-                    if ((!cibaErrorCapabilityExist || !cibaSuccessCapabilityExist)) {
-                        this.addError("dv-er-ciba-flow-001", {
-                            flowId: flow.flowId,
-                        });
-                    }
 
-                    // Second Rule:  logic for flowConductorConnector capabilities
-                    const flowNodes = nodes?.filter(n => n.data.connectorId === 'flowConnector' && n.data?.isDisabled !== true);
-                    const oobStartCapabilityExist = flowNodes?.some(n => n.data.capabilityName === 'oobStart');
-                    const oobContinueCapabilityExist = flowNodes?.some(n => n.data.capabilityName === 'oobContinue');
-                    //Both oobStart and oobContinue are required for CIBA flow execution, if any of them is missing then throw error.
-
-                    if ((!oobStartCapabilityExist || !oobContinueCapabilityExist)) {
-                        this.addError("dv-er-ciba-flow-002", {
-                            flowId: flow.flowId,
-                        });
-                    }
-
-                    // Third Rule:  logic for validating that CIBA response nodes are not present in subflows
-                    if(flow.flowId !== targetFlow.flowId){
-                        //Both returnCibaError and returnCibaSuccess  should not be present in Subflows
+                    // Rule: CIBA response nodes presence
+                    if (flow.flowId !== targetFlow.flowId) {
+                        //SubFlow Rule: Both returnCibaError and returnCibaSuccess  should not be present in Subflows
                         if ((cibaErrorCapabilityExist || cibaSuccessCapabilityExist)) {
-                            const flowConnectorNode= mainFlowNodes.filter(n=>n?.data?.properties?.subFlowId?.value?.value === flow.flowId)
+                            const flowConnectorNode = mainFlowExecutionNodes.filter(n => n?.data?.properties?.subFlowId?.value?.value === flow.flowId)
                             this.addError("dv-er-ciba-flow-003", {
                                 flowId: targetFlow.flowId,
                                 nodeId: flowConnectorNode[0].data.id,
                                 recommendationArgs: [`${flow.name}`],
                             });
                         }
+                    } else {
+                        //Mainflow Rule: Both returnCibaError and returnCibaSuccess are required for CIBA flow response, 
+                        //if any of them is missing then throw error.
+                        if ((!cibaErrorCapabilityExist || !cibaSuccessCapabilityExist)) {
+                            this.addError("dv-er-ciba-flow-001", {
+                                flowId: flow.flowId,
+                            });
+                        }
+                    }
+
+                    // Second Rule:  logic for flowConductorConnector capabilities
+                    const flowNodes = nodes?.filter(n => n.data.connectorId === 'flowConnector' && n.data?.isDisabled !== true);
+
+                    //Both oobStart and oobContinue are required for CIBA flow execution, if any of them is missing then throw error.
+                    //These 2 capabilites can be spread over multiple flows
+                    if (flowNodes?.some(n => n.data.capabilityName === 'oobStart')) {
+                        oobStartCapabilityPresent = true;
+                    }
+                    if (flowNodes?.some(n => n.data.capabilityName === 'oobContinue')) {
+                        oobContinueCapabilityPresent = true;
                     }
                 }
+                if ((!oobContinueCapabilityPresent || !oobStartCapabilityPresent)) {
+                    this.addError("dv-er-ciba-flow-002", {
+                        flowId: targetFlow.flowId,
+                    });
+                }
             }
-        } catch(err) {
+        } catch (err) {
             this.addError(undefined, { messageArgs: [`${err}`] });
         }
     }
